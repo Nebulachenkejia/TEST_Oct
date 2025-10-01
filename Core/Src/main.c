e4e86d1305c03fdd300982c1fdb43bd6cb39b908
Nebulachenkejia <jonesnebula1209@gmail.com>
2025-10-01 17:44:38 +0800

    perf:优化按钮逻辑

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -103,19 +103,18 @@ int main(void)
   while (1)
   {
     state = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_2);
-    if (state == GPIO_PIN_SET && last_state==GPIO_PIN_RESET) {
+    if (state == GPIO_PIN_SET) {
         key_time = HAL_GetTick();
-				last_state=GPIO_PIN_SET;
+			 flag += 1;
     }
-    if ((HAL_GetTick() - key_time) >15 && last_state==GPIO_PIN_SET) {
-            flag = !flag;
-					last_state=GPIO_PIN_RESET;
+    if ((HAL_GetTick() - key_time) > 15) {
+        if (state == GPIO_PIN_SET && last_state == GPIO_PIN_RESET) {
+            flag = (flag + 1);
         }
-    
-		
-	
+        last_state = state;
+    }
     if ((HAL_GetTick() - led_time) > interval) {
-        if (flag) {
+        if (flag % 2 == 1) {
             HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_11);
             HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_SET);
         }
@@ -125,6 +124,7 @@ int main(void)
         }
         led_time = HAL_GetTick();
     }
+		HAL_Delay(50);
   }
 	
 
