12d092814e900b6770430d1b916ce8ba6bd51f77
Nebulachenkejia <jonesnebula1209@gmail.com>
2025-10-03 12:04:31 +0800

    feat:电机转动

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -38,6 +38,8 @@
         * Output
         * EVENT_OUT
         * EXTI
+     PE8   ------> UART7_TX
+     PE7   ------> UART7_RX
 */
 void MX_GPIO_Init(void)
 {
@@ -46,6 +48,7 @@ void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
@@ -55,6 +58,14 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOF, GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PE8 PE7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF8_UART7;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PE11 */
   GPIO_InitStruct.Pin = GPIO_PIN_11;
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,14 +18,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "can.h"
 #include "tim.h"
-#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <string.h>
-#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,7 +44,32 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint8_t rx_msg[4];
+uint8_t tx_data[8] = {0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+uint8_t rx_data[8];
+uint32_t can_tx_mail_box_;
+CAN_RxHeaderTypeDef rx_header;
+CAN_TxHeaderTypeDef tx_header = {
+  .StdId = 0x200,
+  .ExtId = 0,
+  .IDE = CAN_ID_STD,
+  .RTR = CAN_RTR_DATA,
+  .DLC = 8,
+  .TransmitGlobalTime = DISABLE,
+};
+
+CAN_FilterTypeDef filter_config = {
+  .FilterIdHigh = 0x0000,
+  .FilterIdLow = 0x0000,
+  .FilterMaskIdHigh = 0x0000,
+  .FilterMaskIdLow = 0x0000,
+  .FilterFIFOAssignment = CAN_FILTER_FIFO0,
+  .FilterBank = 0,
+  .FilterMode = CAN_FILTERMODE_IDMASK,
+  .FilterScale =  CAN_FILTERSCALE_32BIT,
+  .FilterActivation = ENABLE,
+};
+
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -89,11 +112,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM1_Init();
-  MX_UART7_Init();
+  MX_CAN1_Init();
+  MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_RESET);
-	HAL_UART_Receive_IT(&huart7, rx_msg,4);
+  HAL_CAN_ConfigFilter(&hcan1, &filter_config);
+  HAL_CAN_Start(&hcan1);
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+  HAL_TIM_Base_Start_IT(&htim6);
   /* USER CODE END 2 */
 
   /* Infinite loop */
