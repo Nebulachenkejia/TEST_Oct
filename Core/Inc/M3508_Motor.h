18b55278c04d9329760e8997c6b94401ee065027
Nebulachenkejia <jonesnebula1209@gmail.com>
2025-10-18 22:11:46 +0800

    feat:引入内外环pid,实现电流输出计算

--- Core/Inc/M3508_Motor.h
+++ Core/Inc/M3508_Motor.h
@@ -5,32 +5,48 @@
 #ifndef TIMER_M3508_MOTOR_H
 #define TIMER_M3508_MOTOR_H
 #include "main.h"
-#include "pid"
+#include "pid.h"
 
 class M3508_Motor
 {
 private:
+    PID spid_, ppid_;
+    float target_angle_ = 0.0f, fdb_angle_ = 0.0f;
+    float target_speed_ = 0.0f, fdb_speed_ = 0.0f, feedforward_speed_ = 0.0f;
+    float feedforward_intensity_ = 0.0f, output_intensity_ = 0.0f;
+
+    enum
+    {
+        TORQUE,
+        SPEED,
+        POSITION_SPEED,
+    } control_method_;
+
     const float ratio_;
-    float angle_ = 0.f;
     float delta_angle_ = 0.f;
     float ecd_angle_ = 0.f;
     float last_ecd_angle_ = 0.f;
     float delta_ecd_angle_ = 0.f;
-    float rotate_speed_ = 0.f;
     float current_ = 0.f;
     float temp_ = 0.f;
 
     // 编码器相关变量
     uint16_t ecd_ = 0;
-    uint16_t given_current_ = 0;
+    int16_t speed_raw_ = 0;
+    int16_t current_read_ = 0;
+
+    //控制量
+    int16_t given_current_ = 0;
 
 public:
-    explicit M3508_Motor(const float ratio) : ratio_(ratio)
-    {
-    };
-
-    void canRxMsgCallback(const uint8_t rx_data[8]);
+    M3508_Motor(const float ratio);
+    void canRxMsgCallback(const uint8_t rx_data[8]); //获取输入
     float linearMapping(int in, int in_min, int in_max, float out_min, float out_max);
+
+    void handle();
+    void SetPosition(float target_position, float feedforward_speed, float feedforward_intensity);
+    void SetSpeed(float target_speed, float feedforward_intensity);
+    void SetIntensity(float intensity);
 };
 
 #endif //TIMER_M3508_MOTOR_H
--- Core/Src/M3508_Motor.cpp
+++ Core/Src/M3508_Motor.cpp
@@ -1,24 +1,75 @@
 #include "M3508_Motor.h"
 
+M3508_Motor::M3508_Motor(const float ratio) : ratio_(ratio), spid_(30.0f, 0.5f, 0.0f, 10.0f, 2000.0f),
+                                              ppid_(30.0f, 0.5f, 0.0f, 10.0f, 2000.0f), control_method_(TORQUE)
+{
+};
+
 void M3508_Motor::canRxMsgCallback(const uint8_t rx_data[8])
 {
     //位运算解码
     ecd_ = rx_data[0] << 8 | rx_data[1];
     ecd_angle_ = linearMapping(ecd_, 0, 8191, 0, 360);
-    rotate_speed_ = rx_data[2] << 8 | rx_data[3];
-    given_current_ = rx_data[4] << 8 | rx_data[5];
-    current_ = linearMapping(given_current_, -16384, 16384, -20, 20);
+    speed_raw_ = rx_data[2] << 8 | rx_data[3];
+    fdb_speed_ = float(speed_raw_);
+    current_read_ = rx_data[4] << 8 | rx_data[5];
+    current_ = linearMapping(current_read_, -16384, 16384, -20, 20);
     temp_ = rx_data[6];
     //角度解算
     delta_ecd_angle_ = ecd_angle_ - last_ecd_angle_;
-    if (delta_ecd_angle_ > 180) delta_ecd_angle_ = 360 - delta_ecd_angle_;
+    if (delta_ecd_angle_ > 180) delta_ecd_angle_ = delta_ecd_angle_ - 360;
     else if (delta_ecd_angle_ < -180) delta_ecd_angle_ = 360 + delta_ecd_angle_;
     delta_angle_ = delta_ecd_angle_ / ratio_;
     last_ecd_angle_ = ecd_angle_;
-    angle_ += delta_angle_;
+    fdb_angle_ += delta_angle_;
 }
 
 float M3508_Motor::linearMapping(int in, int in_min, int in_max, float out_min, float out_max)
 {
     return (in - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void M3508_Motor::SetPosition(float target_position, float feedforward_speed, float feedforward_intensity)
+{
+    control_method_ = POSITION_SPEED;
+    target_angle_ = target_position;
+    feedforward_speed_ = feedforward_speed;
+    feedforward_intensity_ = feedforward_intensity;
+    spid_.reset();
+    ppid_.reset();
+}
+
+void M3508_Motor::SetSpeed(float target_speed, float feedforward_intensity)
+{
+    control_method_ = SPEED;
+    target_speed_ = target_speed;
+    feedforward_intensity_ = feedforward_intensity;
+    spid_.reset();
+}
+
+void M3508_Motor::SetIntensity(float intensity)
+{
+    control_method_ = TORQUE;
+    feedforward_intensity_ = intensity;
+}
+
+void M3508_Motor::handle()
+{
+    switch (control_method_)
+    {
+    case TORQUE:
+        output_intensity_ = feedforward_intensity_;
+        break;
+    case SPEED:
+        output_intensity_ = spid_.calc(target_speed_, fdb_speed_) + feedforward_intensity_;
+        break;
+    case POSITION_SPEED:
+        target_speed_ = ppid_.calc(target_angle_, fdb_angle_) + feedforward_speed_;
+        output_intensity_ = spid_.calc(target_speed_, fdb_speed_) + feedforward_intensity_;
+        break;
+    }
+    if (output_intensity_ > spid_.out_max_) output_intensity_ = spid_.out_max_;
+    else if (output_intensity_ < -spid_.out_max_) output_intensity_ = -spid_.out_max_;
+    given_current_ = static_cast<int16_t>(linearMapping(output_intensity_, -spid_.out_max_, spid_.out_max_, -16384,
+                                                        16384));
 }
